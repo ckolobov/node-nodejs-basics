c6b75686311be0220b83f186feb572e0cf8c81a1
Constantine Kolobov <kolobovconstantine@gmail.com>
2022-12-04 18:50:39 +0100

    feat: add fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import { readFile } from "fs/promises";
+import { getPath } from "../utils.js";
+
+const filePath = getPath(import.meta.url, '/files/fileToRead.txt');
+
 const read = async () => {
-  // Write your code here
+    try {
+        const data = await readFile(filePath, { encoding: 'utf8' });
+        console.log(data);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
