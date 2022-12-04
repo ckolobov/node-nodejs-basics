6145ce808605e5000e760d170044b648b93e4582
Constantine Kolobov <kolobovconstantine@gmail.com>
2022-12-04 19:12:37 +0100

    feat: add cli env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-  // Write your code here
+    const RSS_vars = [];
+    for (const [key, value] of Object.entries(process.env)) {
+        if (key.slice(0, 4) === 'RSS_') {
+            RSS_vars.push(`${key}=${value}`);
+        }
+    }
+    console.log(RSS_vars.join('; '));
 };
 
 parseEnv();
