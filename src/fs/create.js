6fae2d8cb5e6c79a2fff2b57b8b69f3ec1316483
Constantine Kolobov <kolobovconstantine@gmail.com>
2022-12-04 17:20:49 +0100

    init: start nodejs basics

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import path from 'path';
+import fs from 'fs';
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const filePath = `${__dirname}/files/fresh.txt`;
+
 const create = async () => {
-  // Write your code here
+    if (fs.existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    }
+    fs.appendFile(filePath, 'I am fresh and young', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
 };
 
 await create();
