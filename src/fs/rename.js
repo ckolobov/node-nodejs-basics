ab0958fd8ff753d4d7a5cb0195fca9da30a173a5
Constantine Kolobov <kolobovconstantine@gmail.com>
2022-12-04 18:38:41 +0100

    feat: add fs copy and rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import { rename as fsRename } from 'fs/promises'
+import {getPath} from "../utils.js";
+import fs from "fs";
+
+const wrongFile = getPath(import.meta.url, '/files/wrongFilename.txt')
+const newFile = getPath(import.meta.url, '/files/properFilename.md')
+
 const rename = async () => {
-  // Write your code here
+    try {
+        if (fs.existsSync(newFile)) {
+            throw new Error('FS operation failed');
+        }
+        await fsRename(wrongFile, newFile)
+    } catch {
+        throw new Error('FS operation failed')
+    }
 };
 
 await rename();
