1439faadc65884ea47b76ec76c2c568da9a17936
Constantine Kolobov <kolobovconstantine@gmail.com>
2022-12-04 21:27:40 +0100

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import { getPath } from "../utils.js";
+import crypto from 'crypto';
+import { readFile } from "fs/promises";
+
 const calculateHash = async () => {
-  // Write your code here
+    const filePath = getPath(import.meta.url, '/files/fileToCalculateHashFor.txt');
+    const fileBuffer = await readFile(filePath);
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fileBuffer);
+    const hex = hashSum.digest('hex');
+    console.log(hex);
 };
 
 await calculateHash();
