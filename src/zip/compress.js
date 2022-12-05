81239b5403946c55ecdc398659eebc6eab409467
Constantine Kolobov <kolobovconstantine@gmail.com>
2022-12-05 22:23:27 +0100

    feat: add zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+import {
+    createReadStream,
+    createWriteStream,
+} from "fs";
+import { getPath } from "../utils.js";
+
 const compress = async () => {
-  // Write your code here
+    const fileToCompress = getPath(import.meta.url, '/files/fileToCompress.txt');
+    const archiveLocation = getPath(import.meta.url, '/files/archive.gz');
+    const gzip = createGzip();
+    const source = createReadStream(fileToCompress);
+    const destination = createWriteStream(archiveLocation);
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createUnzip } from "zlib";
+import { pipeline } from "stream";
+import {
+    createReadStream,
+    createWriteStream,
+} from "fs";
+import { getPath } from "../utils.js";
+
 const decompress = async () => {
-  // Write your code here
+    const decompressedFileLocation = getPath(import.meta.url, '/files/fileToCompress.txt');
+    const fileToDecompress = getPath(import.meta.url, '/files/archive.gz');
+    const unzip = createUnzip();
+    const source = createReadStream(fileToDecompress);
+    const destination = createWriteStream(decompressedFileLocation);
+
+    pipeline(source, unzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await decompress();
