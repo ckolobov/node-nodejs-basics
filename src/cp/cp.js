db0bcc323a62cb00b92158ddba9aa2491524e01f
Constantine Kolobov <kolobovconstantine@gmail.com>
2022-12-06 00:06:19 +0100

    feat: add child processed

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { fork } from "child_process";
+import { getPath } from "../utils.js";
+
+const childPath = getPath(import.meta.url, '/files/script.js');
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+    fork(childPath, args ? args.split(' ') : []);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess();
