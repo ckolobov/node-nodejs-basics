9825f70c24137897e6a7f56b82eb49b9b749a3e3
Constantine Kolobov <kolobovconstantine@gmail.com>
2025-10-20 23:19:54 +0200

    feat: update create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,16 +1,14 @@
-import fs from "fs";
+import fs from "fs/promises";
 import { getPath } from "../utils.js";
 
 const filePath = getPath(import.meta.url, '/files/fresh.txt');
 
 const create = async () => {
-    if (fs.existsSync(filePath)) {
+    try {
+        await fs.writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+    } catch (error) {
         throw new Error('FS operation failed');
     }
-    fs.appendFile(filePath, 'I am fresh and young', function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-    });
 };
 
 await create();
