5f259a5d27449547f2904633b1fa2b5565fb6a8f
Constantine Kolobov <kolobovconstantine@gmail.com>
2025-10-21 23:48:42 +0200

    feat: update calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,14 +1,24 @@
 import { getPath } from "../utils.js";
 import crypto from 'crypto';
-import { readFile } from "fs/promises";
+import { createReadStream } from 'fs';
 
 const calculateHash = async () => {
     const filePath = getPath(import.meta.url, '/files/fileToCalculateHashFor.txt');
-    const fileBuffer = await readFile(filePath);
     const hashSum = crypto.createHash('sha256');
-    hashSum.update(fileBuffer);
-    const hex = hashSum.digest('hex');
-    console.log(hex);
+    const readStream = createReadStream(filePath);
+
+    readStream.on('data', (chunk) => {
+        hashSum.update(chunk);
+    });
+
+    readStream.on('end', () => {
+        const hex = hashSum.digest('hex');
+        console.log(hex);
+    });
+
+    readStream.on('error', (error) => {
+        console.error('Error reading file:', error);
+    });
 };
 
 await calculateHash();
