5fb242d476678c6da823a1bf21d7d30e02c1d42a
Constantine Kolobov <kolobovconstantine@gmail.com>
2025-10-22 21:44:13 +0200

    feat: update spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,12 +1,15 @@
-import { fork } from "child_process";
+import { spawn } from "child_process";
 import { getPath } from "../utils.js";
 
 const childPath = getPath(import.meta.url, '/files/script.js');
 
 const spawnChildProcess = async (args) => {
-    fork(childPath, args ? args.split(' ') : []);
+    const child = spawn('node', [childPath, ...(args || [])]);
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-// spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// spawnChildProcess([someArgument1, someArgument2, ...]);
 await spawnChildProcess();
